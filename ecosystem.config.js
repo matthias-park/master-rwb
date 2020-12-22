a99b53b22a3d11392ddca60700ccaf36a5b0a029
Mantas Buga <mantasbuga97@gmail.com>
2020-12-22 15:59:28 +0200

    [TBR-9312] deployment files #7

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,9 @@ module.exports = {
       env: {
         NODE_ENV: 'development',
       },
+      env_stage: {
+        NODE_ENV: 'stage',
+      },
       env_production: {
         NODE_ENV: 'production',
       },
