029584dfea48a49cafdfa33f1d03cb1da4cc1e9c
Mantas Buga <mantasbuga97@gmail.com>
2021-01-25 12:01:42 +0200

    express basic auth + temporary send basic auth credentials to rails

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 const config = require('config');
 const express = require('express');
 const path = require('path');
+const auth = require('basic-auth');
+const fs = require('fs');
 
 const app = express();
 
@@ -8,6 +10,41 @@ const domainsToName = config.get('franchises').reduce((obj, fr) => {
   obj[fr.domain] = fr.name;
   return obj;
 }, {});
+const basicAuth = config.has('basicAuth') && config.get('basicAuth');
+
+app.set('trust proxy', true);
+
+app.use((req, res, next) => {
+  if (
+    !basicAuth ||
+    !basicAuth.users ||
+    !basicAuth.whitelistedIp ||
+    basicAuth.whitelistedIp.includes(req.ip)
+  ) {
+    return next();
+  }
+  const authentication = auth(req);
+  const unauthorized = () => {
+    if (!authentication) {
+      const challengeString = 'Basic realm="Application"';
+      res.set('WWW-Authenticate', challengeString);
+    }
+    return res.status(401).send('Unauthorized!');
+  };
+  if (!authentication) {
+    return unauthorized();
+  }
+  for (var i in basicAuth.users) {
+    if (
+      authentication.name === i &&
+      authentication.pass === basicAuth.users[i]
+    ) {
+      req.auth = authentication;
+      return next();
+    }
+  }
+  return unauthorized();
+});
 
 app.use((req, _, next) => {
   if (req.path.includes('/assets/')) {
@@ -19,7 +56,22 @@ app.use((req, _, next) => {
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
   const name = domainsToName[req.hostname];
-  res.sendFile(path.join(__dirname, `/build/${name}.html`));
+  const filePath = path.join(__dirname, `/build/${name}.html`);
+  if (req.auth) {
+    fs.readFile(filePath, 'utf8', function (err, data) {
+      if (err) {
+        console.log(err);
+        return res.send('hi!');
+      }
+      const withAuthContent = data.replace(
+        'API_URL="https://',
+        `API_URL="https://${req.auth.name}:${req.auth.pass}@`,
+      );
+      res.send(withAuthContent);
+    });
+  } else {
+    res.sendFile(path.join(__dirname, `/build/${name}.html`));
+  }
 });
 
 const port = config.get('port') || 3800;
