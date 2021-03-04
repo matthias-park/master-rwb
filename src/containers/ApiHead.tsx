5d8e8e6250ab103bb35564437debeff73424c2ed
Mantas Buga <mantasbuga97@gmail.com>
2021-03-04 14:30:49 +0200

    added open graph protocol for bots

--- src/containers/ApiHead.tsx
+++ src/containers/ApiHead.tsx
@@ -6,12 +6,13 @@ import useSWR from 'swr';
 import SeoPages from '../types/api/content/SeoPages';
 import { postApi } from '../utils/apiUtils';
 import RailsApiResponse from '../types/api/RailsApiResponse';
+import { useI18n } from '../hooks/useI18n';
 
 const ApiHead = () => {
-  const { locale } = useConfig();
-  // const { t } = useI18n();
+  const { locale, routes } = useConfig();
+  const { t } = useI18n();
   const { pathname } = useLocation();
-  // const pathInfo = routes.find(route => route.path === pathname);
+  const pathInfo = routes.find(route => route.path === pathname);
   const params = useMemo(
     () => ({
       slug: pathname,
@@ -27,27 +28,34 @@ const ApiHead = () => {
     },
   );
   const seoData = data?.Success ? data?.Data : null;
-  // const fallbackTitle =
-  //   t(`sitemap_${pathInfo?.name}`) + ' - ' + t('seo_site_name');
+  const fallbackTitle =
+    t(`sitemap_${pathInfo?.name}`) + ' - ' + t('seo_site_name');
   return (
     <>
       <Helmet htmlAttributes={{ lang: locale }}>
         {!!seoData ? (
           <>
             <title>{seoData.title} </title>
-            {['description', 'keywords'].map(id => (
-              <meta key={id} name={id} content={seoData[id]} />
-            ))}
+            <meta name="description" content={seoData.description} />
+            <meta name="keywords" content={seoData.keywords} />
             {!!seoData.canonical_tag && (
               <link rel="canonical" href={seoData.canonical_tag} />
             )}
+
+            <meta property="og:title" content={seoData.title} />
           </>
         ) : (
           <>
             {/* <title>{fallbackTitle}</title>
             <meta name="description" content={t('seo_description')} /> */}
+            <meta property="og:title" content={fallbackTitle} />
           </>
         )}
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={window.location.href} />
+        <meta property="og:site_name" content={t('seo_site_name')} />
+        <meta property="og:image" content="/assets/images/logo/logo.svg" />
       </Helmet>
       {!!seoData?.hidden_h1 && (
         <h1 style={{ display: 'none' }}>{seoData.hidden_h1}</h1>
