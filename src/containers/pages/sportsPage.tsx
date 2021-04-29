be8417d7a2c417ab1995b0a5fb3efb1535ff1025
Mantas Buga <mantasbuga97@gmail.com>
2021-04-29 10:56:54 +0300

    sportsbook load after language set

--- src/containers/pages/sportsPage.tsx
+++ src/containers/pages/sportsPage.tsx
@@ -1,3 +1,10 @@
+import React from 'react';
+import { useConfig } from '../../hooks/useConfig';
 import KambiSportsbook from '../KambiSportsbook';
 
-export default KambiSportsbook;
+const SportsPage = () => {
+  const { locale } = useConfig((prev, next) => !!prev.locale === !!next.locale);
+  if (!locale) return null;
+  return <KambiSportsbook />;
+};
+export default SportsPage;
