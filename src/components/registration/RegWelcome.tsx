2789b42b113c0bf0fd0783cdda7a4d86dfbae71f
Mantas Buga <mantasbuga97@gmail.com>
2021-05-05 10:06:38 +0300

    success registration deposit link fix

--- src/components/registration/RegWelcome.tsx
+++ src/components/registration/RegWelcome.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { PagesName } from '../../constants';
+import { useRoutePath } from '../../hooks';
 import { useI18n } from '../../hooks/useI18n';
 import Link from '../Link';
 
 const RegWelcome = () => {
   const { t } = useI18n();
-
+  const depositPath = useRoutePath(PagesName.DepositPage, true);
   return (
     <div className="reg-welcome">
       <h1 className="reg-welcome__title">{t('reg_welcome_title')}</h1>
@@ -17,7 +19,7 @@ const RegWelcome = () => {
           <p className="action-block__text mb-3">
             {t('reg_welcome_action_text')}
           </p>
-          <Link to="/deposit" className="btn btn-primary">
+          <Link to={depositPath} className="btn btn-primary">
             {t('reg_welcome_action_button')}
           </Link>
         </div>
