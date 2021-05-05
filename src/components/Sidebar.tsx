4b967b2c59720ad557cafbf18253e4424f968b97
Motiejus <motiejus.labzentis@tglab.com>
2021-05-05 09:06:14 +0300

    open grouping by path

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import clsx from 'clsx';
 import { useI18n } from '../hooks/useI18n';
@@ -19,67 +19,79 @@ interface Props {
 const Sidebar = ({ links }: Props) => {
   const { pathname } = useLocation();
   const { t } = useI18n();
-  const [expandedGrouping, setExpandedGrouping] = useState<string | null>(null);
+  const [expandedGrouping, setExpandedGrouping] = useState('');
+
+  useEffect(() => {
+    const pathGroup = links.find(
+      link =>
+        link.children?.length &&
+        link.children?.some(child => pathname.includes(child.link)),
+    );
+    pathGroup && setExpandedGrouping(pathGroup?.name);
+  }, [pathname]);
 
   return (
     <div className="left-sidebar sticky">
       <ul className="sidebar-list">
-        {links.map(link =>
-          link.children ? (
-            <Accordion key={link.link}>
-              <Accordion.Toggle
-                eventKey={link.name}
-                as="li"
-                onClick={() =>
-                  setExpandedGrouping(
-                    expandedGrouping === link.name ? null : link.name,
-                  )
-                }
+        <Accordion activeKey={expandedGrouping}>
+          {links.map(link =>
+            link.children ? (
+              <>
+                <Accordion.Toggle
+                  key={link.link}
+                  eventKey={link.name}
+                  as="li"
+                  onClick={() =>
+                    setExpandedGrouping(
+                      expandedGrouping === link.name ? '' : link.name,
+                    )
+                  }
+                  className={clsx(
+                    'd-flex sidebar-list__item',
+                    link.name === expandedGrouping && 'show',
+                  )}
+                >
+                  {t(link.name)}
+                  <i className="icon-down1 ml-auto sidebar-list__item-icon"></i>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey={link.name}>
+                  {link.children && (
+                    <ul className="sidebar-list__sub">
+                      {link.children.map(childLink => (
+                        <li
+                          key={childLink.link}
+                          className={clsx(
+                            `sidebar-list__sub-item`,
+                            childLink.link === pathname ? 'active' : 'inactive',
+                          )}
+                        >
+                          <Link
+                            to={childLink.link}
+                            className="sidebar-list__sub-item-link"
+                          >
+                            {t(childLink.name)}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </Accordion.Collapse>
+              </>
+            ) : (
+              <li
+                key={link.link}
                 className={clsx(
-                  'd-flex sidebar-list__item',
-                  link.name === expandedGrouping && 'show',
+                  `sidebar-list__item`,
+                  link.link === pathname ? 'active' : 'inactive',
                 )}
               >
-                {t(link.name)}
-                <i className="icon-down1 ml-auto sidebar-list__item-icon"></i>
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey={link.name}>
-                {link.children && (
-                  <ul className="sidebar-list__sub">
-                    {link.children.map(childLink => (
-                      <li
-                        key={childLink.link}
-                        className={clsx(
-                          `sidebar-list__sub-item`,
-                          childLink.link === pathname ? 'active' : 'inactive',
-                        )}
-                      >
-                        <Link
-                          to={childLink.link}
-                          className="sidebar-list__sub-item-link"
-                        >
-                          {t(childLink.name)}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </Accordion.Collapse>
-            </Accordion>
-          ) : (
-            <li
-              key={link.link}
-              className={clsx(
-                `sidebar-list__item`,
-                link.link === pathname ? 'active' : 'inactive',
-              )}
-            >
-              <Link to={link.link} className="sidebar-list__item-link">
-                {t(link.name)}
-              </Link>
-            </li>
-          ),
-        )}
+                <Link to={link.link} className="sidebar-list__item-link">
+                  {t(link.name)}
+                </Link>
+              </li>
+            ),
+          )}
+        </Accordion>
       </ul>
     </div>
   );
