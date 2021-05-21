ce0de10310f8cefd4cbb1e7f3a078cfbe5b750cc
Motiejus <motiejus.labzentis@tglab.com>
2021-05-21 14:00:30 +0300

    update for gdpr settings view

--- src/containers/bnl/pages/communicationPreferencesPage.tsx
+++ src/containers/bnl/pages/communicationPreferencesPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, { useState, useMemo, useCallback, useEffect } from 'react';
 import { useI18n } from '../../../hooks/useI18n';
 import CustomToggleCheck from '../components/CustomToggleCheck';
 import Spinner from 'react-bootstrap/Spinner';
@@ -14,7 +14,6 @@ import { useAuth } from '../../../hooks/useAuth';
 import { postApi } from '../../../utils/apiUtils';
 
 interface CommunicationPrefProps {
-  id: string;
   title: string;
   subText: string;
   fields: {
@@ -23,33 +22,88 @@ interface CommunicationPrefProps {
     type: string;
     default: number;
   }[];
-  action: string;
+  setValues: (number) => void;
+  values: {
+    [key: string]: number;
+  };
 }
 
 const CommunicationPrefCard = ({
-  id,
   title,
   subText,
   fields,
-  action,
+  setValues,
+  values,
 }: CommunicationPrefProps) => {
+  const { t } = useI18n();
+
+  return (
+    <li className="communication-prefs__pref">
+      <h6 className="communication-prefs__pref-title">{t(title)}</h6>
+      <p className="communication-prefs__pref-text">{t(subText)}</p>
+      {fields.map(field => (
+        <div className="communication-prefs__pref-item" key={field.id}>
+          <span>{t(field.title)}</span>
+          <span className="ml-auto d-flex align-items-center">
+            <CustomToggleCheck
+              id={t(field.id)}
+              checked={values[field.id] === 1 ? true : false}
+              onChange={() =>
+                setValues({
+                  ...values,
+                  [field.id]: values[field.id] === 1 ? 0 : 1,
+                })
+              }
+            />
+          </span>
+        </div>
+      ))}
+    </li>
+  );
+};
+
+const CommunicationPreferencesPage = () => {
   const { addToast } = useToasts();
   const { user } = useAuth();
   const { t } = useI18n();
-  const { handleSubmit, formState } = useForm();
-  const [checkboxValues, setCheckboxValues] = useState(
-    fields
-      .filter(field => field.default !== undefined && field.default !== null)
-      .reduce((obj, value) => {
-        obj[value.id] = Boolean(value.default);
-        obj[value.id] = value.default?.toString();
-        return obj;
-      }, {}),
+  const { data, error } = useApi<any>(
+    '/railsapi/v1/user/profile/communication_preferences',
   );
+  const { handleSubmit, formState } = useForm();
+  const [checkboxValues, setCheckboxValues] = useState({});
   const [apiResponse, setApiResponse] = useState<{
     success: boolean;
     msg: string;
   } | null>(null);
+  const isDataLoading = !data && !error;
+  const questionItems = useMemo(
+    () => [
+      {
+        title: t('communications_question_1'),
+        body: t('communications_answer_1'),
+      },
+      {
+        title: t('communications_question_2'),
+        body: t('communications_answer_2'),
+      },
+    ],
+    [t],
+  );
+
+  useEffect(() => {
+    data?.blocks &&
+      setCheckboxValues(
+        data.blocks
+          .reduce((acc, block) => acc.concat(block.fields), [])
+          .filter(
+            field => field.default !== undefined && field.default !== null,
+          )
+          .reduce((obj, value) => {
+            obj[value.id] = value.default;
+            return obj;
+          }, {}),
+      );
+  }, [data]);
 
   const handleOnSubmit = async (
     url: string,
@@ -62,7 +116,7 @@ const CommunicationPrefCard = ({
       formData: formBody,
     }).catch((res: RailsApiResponse<null>) => {
       if (res.Fallback) {
-        addToast(`Failed to update user settings`, {
+        addToast(`Failed to update user gdpr settings`, {
           appearance: 'error',
           autoDismiss: true,
         });
@@ -83,94 +137,61 @@ const CommunicationPrefCard = ({
         return obj;
       }, {}),
     };
-    return handleOnSubmit(action, body);
+    return handleOnSubmit(data.action, body);
   }, [checkboxValues]);
 
   return (
-    <li className="communication-prefs__pref">
-      <form onSubmit={handleSubmit(updateSettingsSubmit)}>
-        <CustomAlert
-          show={!!apiResponse}
-          variant={
-            (apiResponse && (apiResponse.success ? 'success' : 'danger')) || ''
-          }
-          className="mb-3"
-        >
-          <div dangerouslySetInnerHTML={{ __html: apiResponse?.msg || '' }} />
-        </CustomAlert>
-        <h6 className="communication-prefs__pref-title">{t(title)}</h6>
-        <p className="communication-prefs__pref-text">{t(subText)}</p>
-        {fields.map(field => (
-          <div className="communication-prefs__pref-item">
-            <span>{t(field.title)}</span>
-            <span className="ml-auto d-flex align-items-center">
-              <CustomToggleCheck
-                id={t(field.id)}
-                checked={checkboxValues[field.id] === 1 ? true : false}
-                onChange={() =>
-                  setCheckboxValues({
-                    ...checkboxValues,
-                    [field.id]: checkboxValues[field.id] === 1 ? 0 : 1,
-                  })
-                }
-              />
-            </span>
-          </div>
-        ))}
-        <LoadingButton
-          loading={formState.isSubmitting}
-          className="mt-3"
-          variant="primary"
-          type="submit"
-        >
-          {t('communications_submit')}
-        </LoadingButton>
-      </form>
-    </li>
-  );
-};
-
-const CommunicationPreferencesPage = () => {
-  const { t } = useI18n();
-  const { data, error } = useApi<any>(
-    '/railsapi/v1/user/profile/communication_preferences',
-  );
-  const isDataLoading = !data && !error;
-  const questionItems = useMemo(
-    () => [
-      { title: t('deposit_question_1'), body: 'deposit_answer_1' },
-      { title: t('deposit_question_2'), body: 'deposit_answer_2' },
-    ],
-    [t],
-  );
-
-  return (
     <main className="container-fluid px-0 px-0 px-sm-4 pl-md-5 mb-4 pt-5">
-      <h1>{t('communication_preferences_page_title')}</h1>
-      <p className="mb-4">{t('communication_preferences_page_sub_text')}</p>
-      {isDataLoading && (
-        <div className="d-flex justify-content-center pt-4 pb-3">
-          <Spinner animation="border" variant="black" className="mx-auto" />
-        </div>
-      )}
-      {!!error && (
-        <h2 className="mt-3 mb-5 text-center">
-          {t('settings_page_failed_to_load')}
-        </h2>
-      )}
-      {!!data && (
-        <ul className="list-unstyled communication-prefs">
-          {data.blocks.map(block => (
-            <CommunicationPrefCard
-              id={block.id}
-              title={block.title}
-              subText={block.note}
-              fields={block.fields}
-              action={block.action}
-            />
-          ))}
-        </ul>
-      )}
+      <form onSubmit={handleSubmit(updateSettingsSubmit)}>
+        <h1>{t('communication_preferences_page_title')}</h1>
+        <p className="mb-4">{t('communication_preferences_page_sub_text')}</p>
+        {isDataLoading && (
+          <div className="d-flex justify-content-center pt-4 pb-3">
+            <Spinner animation="border" variant="black" className="mx-auto" />
+          </div>
+        )}
+        {!!error && (
+          <h2 className="mt-3 mb-5 text-center">
+            {t('settings_page_failed_to_load')}
+          </h2>
+        )}
+        {!!data && (
+          <div className="communication-prefs-wrp">
+            <CustomAlert
+              show={!!apiResponse}
+              variant={
+                (apiResponse && (apiResponse.success ? 'success' : 'danger')) ||
+                ''
+              }
+              className="mb-0"
+            >
+              <div
+                dangerouslySetInnerHTML={{ __html: apiResponse?.msg || '' }}
+              />
+            </CustomAlert>
+            <div className="list-unstyled communication-prefs">
+              {data.blocks.map(block => (
+                <CommunicationPrefCard
+                  key={block.id}
+                  title={block.title}
+                  subText={block.note}
+                  fields={block.fields}
+                  values={checkboxValues}
+                  setValues={setCheckboxValues}
+                />
+              ))}
+            </div>
+            <LoadingButton
+              loading={formState.isSubmitting}
+              className="mt-3"
+              variant="primary"
+              type="submit"
+            >
+              {t('communications_submit')}
+            </LoadingButton>
+          </div>
+        )}
+      </form>
       <QuestionsContainer items={questionItems} className="mt-5" />
       <HelpBlock
         title={'user_help_title'}
