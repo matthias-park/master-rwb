25eb481c9b84d0fd6c6e2345f60978fb41993726
Mantas Buga <mantasbuga97@gmail.com>
2021-06-07 10:12:18 +0300

    [TBR-10427] kambi event statistics

--- src/containers/KambiSportsbook.tsx
+++ src/containers/KambiSportsbook.tsx
@@ -159,6 +159,7 @@ interface SetCustomerSettingsProps {
   getApiBalance: string;
   updateBalance: () => void;
   setKambiLoaded: () => void;
+  locale: string;
 }
 
 interface KambiSportsbookProps {
@@ -187,6 +188,7 @@ const setCustomerSettings = ({
   getApiBalance,
   updateBalance,
   setKambiLoaded,
+  locale,
 }: SetCustomerSettingsProps) => {
   window.customerSettings = {
     getBalance: function (successFunc, failureFunc) {
@@ -206,6 +208,11 @@ const setCustomerSettings = ({
       ) {
         setKambiLoaded();
       }
+    },
+    showEventStatistics: function (eventId) {
+      const url = `https://s5.sir.sportradar.com/scooorebe/be/match/m${eventId}?language=${locale}`;
+      const newStatisticsTab = window.open(url, '_blank');
+      newStatisticsTab?.focus();
     },
     hideHeader: true,
     enableOddsFormatSelector: true,
@@ -279,6 +286,7 @@ const KambiSportsbook = () => {
             getApiBalance: kambiConfig?.getApiBalance,
             updateBalance: () => updateUser(),
             setKambiLoaded: () => context.setSportsbookLoaded(true),
+            locale,
           });
           const kambiContainer = document.createElement('div');
           kambiContainer.id = 'KambiBC';
