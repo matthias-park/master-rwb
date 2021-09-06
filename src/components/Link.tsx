5654617ffddb44b82140fe7d42422a59da54bd18
James Larson <timothy.james@tglab.com>
2021-09-06 10:41:56 +0300

    [TBR-10753] faq link directs to betsson
    Summary: [TBR-10753] faq links direct correctly
    
    Test Plan: Review
    
    Reviewers: mantas.buga
    
    Reviewed By: mantas.buga
    
    Subscribers: mantas.buga
    
    Differential Revision: https://ph.tonybet.com/D17426

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
-const Link: typeof RouterLink = React.forwardRef((props, ref) => (
-  <RouterLink
-    {...props}
-    ref={ref}
-    onClick={e => {
-      if (window._wbUpdate) window.location.pathname = props.to.toString();
-      if (props.onClick) props.onClick(e);
-    }}
-  />
-));
+const Link: typeof RouterLink = React.forwardRef((props, ref) => {
+  return props.to.toString().includes('https://') ? (
+    <a
+      href={props.to.toString()}
+      ref={ref}
+      {...props}
+      onClick={e => props.onClick?.(e)}
+    >
+      {props.children}
+    </a>
+  ) : (
+    <RouterLink
+      {...props}
+      ref={ref}
+      onClick={e => {
+        if (window._wbUpdate) window.location.pathname = props.to.toString();
+        if (props.onClick) props.onClick(e);
+      }}
+    />
+  );
+});
 
 export default Link;
