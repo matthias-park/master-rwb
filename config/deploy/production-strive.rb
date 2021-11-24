4c5d7b78ca57b55c5190668ca8d916fa5a7ffb95
Mantas Buga <mantasbuga97@gmail.com>
2021-11-24 15:00:23 +0200

    strive prod configuration
    Summary: Review
    
    Test Plan: Review
    
    Reviewers: motiejus.labzentis
    
    Reviewed By: motiejus.labzentis
    
    Differential Revision: https://ph.tonybet.com/D19002

--- config/deploy/production-strive.rb
+++ config/deploy/production-strive.rb
@@ -1,12 +1,13 @@
 require 'net/ssh/proxy/command'
 
-server '192.168.86.21', user: 'deploy', roles: %w(next-web prod-release)
+server '192.168.60.21', user: 'deploy', roles: %w(next-web prod-release)
+server '192.168.60.22', user: 'deploy', roles: %w(next-web prod-release)
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/deploy/next-web'
 
 set :nvm_type, :user
-set :nvm_node, 'v12.22.2'
+set :nvm_node, 'v12.22.6'
 set :nvm_map_bins, %w{node npm pm2}
 set :build_env, 'production'
 
