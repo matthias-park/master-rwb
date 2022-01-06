e12aa17171a3ca370df2a512f87daafd23dda509
Motiejus <motiejus.labzentis@tglab.com>
2022-01-06 15:15:36 +0200

    TBR-11620 refetch banners on log in
    Test Plan: TBR-11620
    
    Reviewers: mantas.buga
    
    Reviewed By: mantas.buga
    
    Differential Revision: https://ph.tonybet.com/D19421

--- src/containers/xcasino/components/Banner.tsx
+++ src/containers/xcasino/components/Banner.tsx
@@ -69,7 +69,7 @@ const Banner = ({ promoSlides, onLoad, onError, zone }: BannerProps) => {
         setSlides(data.Data);
       })();
     }
-  }, [promoSlides]);
+  }, [promoSlides, user]);
 
   return (
     <div className="main-banner banner fixed-aspect">
