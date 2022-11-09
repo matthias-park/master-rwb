c3c99d0e4824ff2439efc25cca71e3f3292a5a57
Junsoo <junsoo@strivegaming.com>
2022-11-09 14:04:57 -0800

    app-links config fix

--- config/stage-desertDiamond.js
+++ config/stage-desertDiamond.js
@@ -10,89 +10,89 @@ module.exports = {
       ],
       appLinks: {
         apple: {
-          webCredentials: {
-            appLinks: {
-              details: [
-                {
-                  appIds: [
-                    '346TY947DH.com.strivegaming.desertDiamondApp',
-                    'TM75T75K7R.com.desertdiamond.desertdiamondsports',
-                  ],
-                  components: [
-                    {
-                      '/': '/$(lang)/reset-password/*',
-                      comment:
-                        'Matches any URL whose path starts with reset password path',
-                    },
-                    {
-                      '/': '/$(lang)/login',
-                      comment:
-                        'Matches any URL whose path starts with login path',
-                    },
-                    {
-                      '/': '/$(lang)/registration/register',
-                      comment:
-                        'Matches any URL whose path starts with registration path',
-                    },
-                    {
-                      '/': '/$(lang)/help/contact-us',
-                      comment:
-                        'Matches any URL whose path starts with contact path',
-                    },
-                    {
-                      '/': '/$(lang)/account/wallet/deposit',
-                      comment:
-                        'Matches any URL whose path starts with deposit path',
-                    },
-                    {
-                      '/': '/$(lang)/account/wallet/withdraw',
-                      comment:
-                        'Matches any URL whose path starts with withdraw path',
-                    },
-                    {
-                      '/': '/$(lang)/promotions',
-                      comment:
-                        'Matches any URL whose path starts with promotions path',
-                    },
-                    {
-                      '/': '/$(lang)/bonuses',
-                      comment:
-                        'Matches any URL whose path starts with bonuses path',
-                    },
-                    {
-                      '/': '/$(lang)/account/profile/personal-info',
-                      comment:
-                        'Matches any URL whose path starts with personal info path',
-                    },
-                    {
-                      '/': '/$(lang)/account/wallet/transactions',
-                      comment:
-                        'Matches any URL whose path starts with transactions path',
-                    },
-                    {
-                      '/': '/$(lang)/responsible-gaming',
-                      comment:
-                        'Matches any URL whose path starts with responsible gaming path',
-                    },
-                    {
-                      '/': '/$(lang)/account/profile/tax-information',
-                      comment:
-                        'Matches any URL whose path starts with tax path',
-                    },
-                    {
-                      '/': '/$(lang)/account/profile/document-center',
-                      comment:
-                        'Matches any URL whose path starts with document center path',
-                    },
-                  ],
-                },
-              ],
-            },
+          webcredentials: {
             apps: [
               '346TY947DH.com.strivegaming.desertDiamondApp',
               'TM75T75K7R.com.desertdiamond.desertdiamondsports',
             ],
           },
+          applinks: {
+            details: [
+              {
+                appIDs: [
+                  '346TY947DH.com.strivegaming.desertDiamondApp',
+                  'TM75T75K7R.com.desertdiamond.desertdiamondsports',
+                ],
+                components: [
+                  {
+                    '/': '/$(lang)/reset-password/*',
+                    comment:
+                      'Matches any URL whose path starts with reset password path',
+                  },
+                  {
+                    '/': '/$(lang)/login',
+                    comment:
+                      'Matches any URL whose path starts with login path',
+                  },
+                  {
+                    '/': '/$(lang)/registration/register',
+                    comment:
+                      'Matches any URL whose path starts with registration path',
+                  },
+                  {
+                    '/': '/$(lang)/help/contact-us',
+                    comment:
+                      'Matches any URL whose path starts with contact path',
+                  },
+                  {
+                    '/': '/$(lang)/account/wallet/deposit',
+                    comment:
+                      'Matches any URL whose path starts with deposit path',
+                  },
+                  {
+                    '/': '/$(lang)/account/wallet/withdraw',
+                    comment:
+                      'Matches any URL whose path starts with withdraw path',
+                  },
+                  {
+                    '/': '/$(lang)/promotions',
+                    comment:
+                      'Matches any URL whose path starts with promotions path',
+                  },
+                  {
+                    '/': '/$(lang)/bonuses',
+                    comment:
+                      'Matches any URL whose path starts with bonuses path',
+                  },
+                  {
+                    '/': '/$(lang)/account/profile/personal-info',
+                    comment:
+                      'Matches any URL whose path starts with personal info path',
+                  },
+                  {
+                    '/': '/$(lang)/account/wallet/transactions',
+                    comment:
+                      'Matches any URL whose path starts with transactions path',
+                  },
+                  {
+                    '/': '/$(lang)/responsible-gaming',
+                    comment:
+                      'Matches any URL whose path starts with responsible gaming path',
+                  },
+                  {
+                    '/': '/$(lang)/account/profile/tax-information',
+                    comment:
+                      'Matches any URL whose path starts with tax path',
+                  },
+                  {
+                    '/': '/$(lang)/account/profile/document-center',
+                    comment:
+                      'Matches any URL whose path starts with document center path',
+                  },
+                ],
+              },
+            ],
+        }
         },
         android: [
           {
