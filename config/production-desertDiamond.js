32aecc337182a82b62b1e23c04c287b513e0e5d0
Callum Lyall <1648577-BoatPartyJesus@users.noreply.gitlab.com>
2022-11-24 12:49:02 +0000

    fix: add missing Redis configuration Redis configuration was accidentally removed when cherry picking commits. Adding it back in

--- config/production-desertDiamond.js
+++ config/production-desertDiamond.js
@@ -99,6 +99,13 @@ module.exports = {
       },
     },
   },
+  redis: {
+    host: '192.168.90.40',
+    port: 6379,
+    db: 12,
+    prefix: 'v5:reactNodeProd-',
+    password: 'CDVXfY90e4XqO6ZztwH9',
+  },
   sentryDsn:
     'https://5c1d5814ea0d42c2a323b15b1310491b@o1137508.ingest.sentry.io/6190258',
 };
